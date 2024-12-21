8888d742518b30e3d59f62476d91b8578c003ef5
YairAresche <areschelainoyair@gmail.com>
2024-12-21 16:50:09 -0300

    fix: cta, images & text

--- src/constants/receips.ts
+++ src/constants/receips.ts
@@ -1,30 +1,30 @@
 export const FOODITEMS = [
   {
     id: 1,
-    src: "/images/kimchi/recipes1.webp",
-    alt: "Dish 1",
-    title: "Mandu de Tofu BBQ Ahumado y Kimchi",
-    description:
-      "¡Celebrando el Día Nacional del Dumpling con nuestra marca de tofu favorita, Hodo!",
-    url: "/",
-  },
-  {
-    id: 2,
-    src: "/images/kimchi/recipes2.jpg",
-    alt: "Dish 2",
+    src: "/images/receipts/spicy-kimchi-ramen.webp",
+    alt: "Ramen Picante de Kimchi con Tofu",
     title: "Ramen Picante de Kimchi con Tofu",
     description:
       "Nos asociamos con una de nuestras marcas favoritas de fideos y caldos asiáticos, Nona Lim, para crear una comida igualmente fácil y deliciosa.",
-    url: "/",
+    url: "/kimchuski/recetas/ramen-picante-kimchi-tofu",
+  },
+  {
+    id: 2,
+    src: "/images/receipts/smoky-bbq-tofu-kimchi-mandu.jpg",
+    alt: "Mandu de Tofu BBQ Ahumado y Kimchi",
+    title: "Mandu de Tofu BBQ Ahumado y Kimchi",
+    description:
+      "¡Celebrando el Día Nacional del Dumpling con nuestra marca de tofu favorita, Hodo!",
+    url: "/kimchuski/recetas/mandu-de-tofu-bbq-ahumado-y-kimchi",
   },
   {
     id: 3,
-    src: "/images/kimchi/recipes3.jpg",
-    alt: "Dish 3",
+    src: "/images/receipts/spicy-cucumber-salad.jpg",
+    alt: "Wraps de Ensalada de Brócoli y MUU",
     title: "Wraps de Ensalada de Brócoli y MUU",
     description:
       "¡Prueba esta deliciosa receta de wraps de ensalada de una de nuestras autoras de libros de cocina favoritas, Joanne Molinaro, también conocida como The Korean Vegan!",
-    url: "/",
+    url: "/kimchuski/recetas/wraps-de-ensalada-de-brocoli-y-muu",
   },
 ];
 
@@ -107,7 +107,7 @@ export const RECEIPTITEMS = [
   },
   {
     id: 3,
-    src: "/images/kimchi/recipes3.jpg",
+    src: "/images/receipts/brocoli-musalad.webp",
     alt: "Dish 3",
     title: "Wraps de Ensalada de Brócoli y MUU",
     description:
@@ -204,7 +204,7 @@ export const RECEIPTITEMS = [
   },
   {
     id: 5,
-    src: "/images/receipts/spicy-cucumber-salad.webp",
+    src: "/images/receipts/spicy-cucumber-salad.jpg",
     alt: "Ensalada Picante de Pepino",
     title: "Ensalada Picante de Pepino",
     description:
